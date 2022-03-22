a590cc8482dca617e6d04bc771623fc5814ffaee
Aneesh S K <92055517+AneeshSaravuKarekad@users.noreply.github.com>
2022-03-22 12:24:56 -0400

    chore: Add a serverless function to return the calender events

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -1,4 +1,6 @@
 const { google } = require('googleapis');
+// const OAuth2 = google.auth.OAuth2;
+const calendar = google.calendar('v3');
 
 const SCOPES = ['https://www.googleapis.com/auth/calendar.readonly'];
 
@@ -17,7 +19,7 @@ const credentials = {
   ],
 };
 
-const { client_id, client_secret, redirect_uris } = credentials;
+const { client_id, client_secret, redirect_uris, calendar_id } = credentials;
 
 /**
  * @function getAuthURL
@@ -92,4 +94,44 @@ module.exports.getAccessToken = async (event) => {
         body: JSON.stringify(err),
       };
     });
+};
+
+module.exports.getCalendarEvents = async (event) => {
+  const oAuth2Client = new google.auth.OAuth2(
+    client_id,
+    client_secret,
+    redirect_uris[0]
+  );
+  // Decode authorization access token extracted from the URL query
+  const access_token = decodeURIComponent(
+    `${event.pathParameters.access_token}`
+  );
+
+  oAuth2Client.setCredentials({ access_token });
+  return new Promise((resolve, reject) => {
+    calendar.events.list(
+      {
+        calendarId: calendar_id,
+        auth: oAuth2Client,
+        timeMin: new Date().toISOString(),
+        singleEvents: true,
+        orderBy: 'startTime',
+      },
+      (error, response) => {
+        if (error) {
+          reject(error);
+        } else {
+          resolve(response);
+        }
+      }
+    );
+  }).then((results) => {
+    return {
+      statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+      },
+      body: JSON.stringify({ events: results.data.items }),
+    };
+  });
 };
