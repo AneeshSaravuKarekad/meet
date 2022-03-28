ca1f99be8ff0e851bfe204f36bbcb261f2f4c06a
Aneesh S K <92055517+AneeshSaravuKarekad@users.noreply.github.com>
2022-03-27 20:32:46 -0400

    test: Add end-to-end tests

--- src/__tests__/EndToEnd.test.js
+++ src/__tests__/EndToEnd.test.js
@@ -28,7 +28,7 @@ describe('show/hide an event details', () => {
   test('User can expand an event to see its details', async () => {
     await page.click('.event .btn-show-hide');
 
-    const eventDetails = await (await page).$('.event .event__Details');
+    const eventDetails = await page.$('.event .event__Details');
     expect(eventDetails).toBeDefined();
   });
 
@@ -36,5 +36,42 @@ describe('show/hide an event details', () => {
     await page.click('.event .btn-show-hide');
     const eventDetails = await page.$('.event .event__Details');
     expect(eventDetails).toBeNull();
+  });
+});
+
+describe('Filter events by city', () => {
+  let browser;
+  let page;
+
+  jest.setTimeout(30000);
+  beforeAll(async () => {
+    browser = await puppeteer.launch({
+      headless: false,
+      slowMo: 250,
+      ignoreDefaultArgs: ['--disable-extensions'],
+    });
+    page = await browser.newPage();
+    await page.goto('http://localhost:3000/');
+    await page.waitForSelector('.event');
+  });
+
+  afterAll(async () => {
+    browser.close();
+  });
+
+  test('show suggestions when typing', async () => {
+    await page.type('.city', 'Berlin');
+    const suggestion = await page.$('.suggestions');
+
+    expect(suggestion).toBeDefined();
+  });
+
+  test('display 1 event when filtering berlin', async () => {
+    const allSuggestions = await page.$('.suggestions li');
+    await allSuggestions.click();
+
+    const events = await page.$('.event');
+
+    expect(events).not.toBeNull();
   });
 });
