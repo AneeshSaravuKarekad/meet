2e1888377daec8c5548e58a06232f09e57a03915
Aneesh S K <92055517+AneeshSaravuKarekad@users.noreply.github.com>
2022-03-31 15:53:11 -0400

    fix: return type of check token

--- src/api.js
+++ src/api.js
@@ -67,7 +67,7 @@ export const checkToken = async (accessToken) => {
     .then((res) => res.json())
     .catch((error) => error.json());
 
-  return result;
+  return result.error ? false : true;
 };
 
 const removeQuery = () => {
