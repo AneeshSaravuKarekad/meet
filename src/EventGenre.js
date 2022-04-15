416a405713fd3a43c2c1a3dc52792e712f582af2
Aneesh S K <92055517+AneeshSaravuKarekad@users.noreply.github.com>
2022-04-15 13:08:49 -0400

    fix: Piechart

--- src/EventGenre.js
+++ src/EventGenre.js
@@ -4,45 +4,42 @@ import { PieChart, Pie, Cell, Legend, ResponsiveContainer } from 'recharts';
 const EventGenre = ({ events }) => {
   const [data, setData] = useState([]);
 
-  const getData = () => {
-    const genres = ['React', 'JavaScript', 'Node', 'jQuery', 'AngularJS'];
-    const data = genres.map((genre) => {
-      const value = events.filter((event) =>
-        event.summary.split(' ').includes(genre)
-      ).length;
-      return { name: genre, value };
-    });
-    return data;
-  };
-
   useEffect(() => {
+    const getData = () => {
+      const genres = ['React', 'JavaScript', 'Node', 'jQuery', 'AngularJS'];
+      const data = genres.map((genre) => {
+        const value = events.filter(({ summary }) =>
+          summary.includes(genre)
+        ).length;
+        return { name: genre, value };
+      });
+      return data.filter((genre) => {
+        return genre.value > 0;
+      });
+    };
     setData(() => getData());
   }, [events]);
 
-  const cellColors = ['#845EC2', '#FF6F91', '#FF9671', '#FFC75F', 'black'];
+  const COLORS = ['#678fe4', '#b588d5', '#e186b7', '#f39099', '#f0a384'];
 
   return (
     <ResponsiveContainer height={400}>
-      <PieChart width={400} height={400}>
+      <PieChart>
         <Pie
           data={data}
+          cx={'50%'}
+          cy={200}
           labelLine={false}
           outerRadius={80}
-          fill="#8884d8"
           dataKey="value"
           label={({ name, percent }) =>
-            percent !== 0 ? `${name} ${(percent * 100).toFixed(0)}%` : ''
+            `${name} ${(percent * 100).toFixed(0)}%`
           }
-          style={{ marginInline: 'auto' }}
         >
-          {data.map((_, index) => (
-            <Cell
-              key={`cell-${index}`}
-              fill={cellColors[index % cellColors.length]}
-            />
+          {data.map((entry, index) => (
+            <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
           ))}
         </Pie>
-        <Legend verticalAlign="bottom" align="center" />
       </PieChart>
     </ResponsiveContainer>
   );
