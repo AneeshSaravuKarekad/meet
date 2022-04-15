9a0e9769646fc7995773b61a1bf061ee6cb794c3
Aneesh S K <92055517+AneeshSaravuKarekad@users.noreply.github.com>
2022-04-15 14:03:45 -0400

    fix: Add Angular as a genre

--- src/EventGenre.js
+++ src/EventGenre.js
@@ -3,14 +3,21 @@ import { PieChart, Pie, Cell, ResponsiveContainer } from 'recharts';
 
 const EventGenre = ({ events }) => {
   const [data, setData] = useState([]);
-  console.log(events);
+
   useEffect(() => {
     const getData = () => {
-      const genres = ['React', 'JavaScript', 'Node', 'jQuery', 'AngularJS'];
+      const genres = [
+        'React',
+        'JavaScript',
+        'Node',
+        'jQuery',
+        'Angular',
+        'AngularJS',
+      ];
       const data = genres.map((genre) => {
-        const value = events.filter(({ summary }) =>
-          summary.includes(genre)
-        ).length;
+        const value = events.filter(({ summary }) => {
+          return summary.includes(genre);
+        }).length;
         return { name: genre, value };
       });
       return data.filter((genre) => {
