ba606604d7d7c2c8b4e3c97fbd07fb42f671a169
Aneesh S K <92055517+AneeshSaravuKarekad@users.noreply.github.com>
2022-04-15 12:59:25 -0400

    refractor: Event Card styling

--- src/Event.js
+++ src/Event.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Button } from 'react-bootstrap';
+import { Button, Card } from 'react-bootstrap';
 
 export default class Event extends Component {
   constructor() {
@@ -19,26 +19,33 @@ export default class Event extends Component {
     const { event } = this.props;
     const { showDetails } = this.state;
     return (
-      <div className="event">
-        <h2 className="summary">{event.summary}</h2>
-        <p className="startDateTimeZone">
-          {event.start.dateTime}({event.start.timeZone})
-        </p>
-        <p className="location">{event.location}</p>
-        {showDetails && (
-          <div className="event__Details">
-            <h3>About event: </h3>
-            <a href={event.htmlLink}>See details on Google Calendar</a>
-            <p>{event.description}</p>
-          </div>
-        )}
-        <Button
-          className="btn btn-show-hide"
-          onClick={() => this.handleButtonClicked()}
-        >
-          {showDetails ? 'Hide Details' : 'Show Details'}
-        </Button>
-      </div>
+      <Card className="event" style={{ width: '24rem' }}>
+        <Card.Header className="summary">
+          <Card.Title>{event.summary}</Card.Title>
+        </Card.Header>
+        <Card.Body>
+          <p className="startDateTimeZone">
+            {event.start.dateTime}({event.start.timeZone})
+          </p>
+          <p className="location">{event.location}</p>
+          {showDetails && (
+            <div className="event__Details">
+              <h3>About event: </h3>
+              <a href={event.htmlLink}>See details on Google Calendar</a>
+              <p>{event.description}</p>
+            </div>
+          )}
+        </Card.Body>
+        <Card.Footer style={{ textAlign: 'center' }}>
+          <Button
+            className="btn btn-show-hide"
+            onClick={() => this.handleButtonClicked()}
+            style={{ width: '100%' }}
+          >
+            {showDetails ? 'Hide Details' : 'Show Details'}
+          </Button>
+        </Card.Footer>
+      </Card>
     );
   }
 }
